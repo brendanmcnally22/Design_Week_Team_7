0885aa503f2cd474a89b3baf0c2e55287c64e6a7
brendanmcnally22 <brendanmcnally295@gmail.com>
2025-02-11 18:47:23 -0500

    Add Dash Mechanic
    Fix dash code,
    
    Added dash mechanic

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-
     public float speed = 5f;
     public float dashSpeed = 10f;
     public float dashDuration = 0.5f;
     public float dashCooldown = 2f;
 
-    private bool canDash = true;
+    private float dashTimer = 0f;
+    private float coolDownTimer = 0f;
     private bool isDashing = false;
     private Rigidbody2D rb;
     private Vector2 movement;
 
-    public string horizontalInput, verticalInput; // input strings
+    public string horizontalInput, verticalInput;
+    public KeyCode dashKey1 = KeyCode.LeftShift; // First dash key
+    public KeyCode dashKey2 = KeyCode.RightShift; // Second dash key
     private playerCollision playerColl;
-
-
 
     void Start()
     {
@@ -26,50 +26,49 @@ public class playerMovement : MonoBehaviour
         playerColl = GetComponent<playerCollision>();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        if (coolDownTimer > 0)
+        {
+            coolDownTimer -= Time.deltaTime;
+        }
+
+        if (dashTimer > 0)
+        {
+            dashTimer -= Time.deltaTime;
+            if (dashTimer <= 0)
+            {
+                isDashing = false;
+            }
+        }
+
+        if (!isDashing)
+        {
+            movement.x = Input.GetAxisRaw(horizontalInput);
+            movement.y = Input.GetAxisRaw(verticalInput);
+            movement.Normalize();
+
+            // Dash Input (Only if cooldown is over and player is NOT "It")
+            if ((Input.GetKeyDown(dashKey1) || Input.GetKeyDown(dashKey2)) && coolDownTimer <= 0 && !playerColl.isIt)
+            {
+                Dash();
+            }
+        }
+    }
+
+    void FixedUpdate()
     {
         if (!isDashing)
         {
-            movement.x = Input.GetAxisRaw(horizontalInput); // get the horizontal input
-            movement.y = Input.GetAxisRaw(verticalInput); // get the vertical input
-            movement.Normalize(); // prevent diagonal movement from being faster
+            rb.velocity = movement * speed;
         }
-
-        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash&& !playerColl.isIt)
-        {
-            Debug.Log(gameObject.name + "started dashing"); 
-            StartCoroutine(Dash());
-        }
-
     }
 
-     void FixedUpdate()
+    private void Dash()
     {
-        rb.velocity = movement * speed; // move the player
-    }
-
-    private IEnumerator Dash()
-    {
-        Debug.Log(gameObject.name + "dashing");
-        canDash = false;
-        isDashing = true; 
-        rb.velocity = movement * dashSpeed; // move the player
-
-
-        yield return new WaitForSeconds(dashDuration); // wait for the dash duration
-
-
-        isDashing = false;
-        rb.velocity = movement * speed; // reset to normal speed
-        
-
-        Debug.Log(gameObject.name + "stopped dashing waiting for cooldown");
-        yield return new WaitForSeconds(dashDuration);
-
-
-        Debug.Log(gameObject.name + "cooldown finished");
-        canDash = true;
-
+        isDashing = true;
+        dashTimer = dashDuration;
+        coolDownTimer = dashCooldown;
+        rb.velocity = movement * dashSpeed;
     }
 }
